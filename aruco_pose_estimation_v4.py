Removed enum module, corrected Z-axis direction, introduce calculation of correction

aruco_pose_estimation_v4.py
@@ -11,11 +11,26 @@ from enum import Enum
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../mavlink'))
 
+class Target(object):
+    def __init__(self, x, y, z, yaw):
+         self.x = x
+         self.y = y
+         self.z = z
+         self.yaw = yaw
+
+### State machine definitions
+State_waiting_px       = 1
+State_waiting_mark     = 2
+State_waiting_lpos     = 3
+State_waiting_offboard = 4
+State_correcting       = 5 
+State_landing          = 6
+
 ### Define Tag
 id_to_find = 77
 dictionnary_to_use = aruco.DICT_4X4_100
 marker_size  = 20 ### [cm]
-calib_path  = "./camera_01/"
+calib_path  = "./camera_02/"
 calibration_file = 'cameraMatrix.txt'
 distortion_file = 'cameraDistortion.txt'
 camera_frame_width = 640
@@ -35,23 +50,24 @@ marker_visible = False
 lpos_received_time = 0
 app_should_stop = False
 copter_mode = 0
+# Landing precision parameters
+x_precision = 0.1
+y_precision = 0.1
+z_precision = 0.25
+yaw_precision = 0.2
+
+### define landing target
+prelanding_target = Target(-1.6,0,0,0)
+landing_target = Target(-1.6,0,5,0)
 
 ###CONSTATNS
 MIN_OBSERVATION_PERIOD = 200   #millis
 LPOS_OBSERVATION_MILLIS = 8000 #millis
 COPTER_SYS_ID = 1
 LPOS_TYPE_MASK = 8 + 16 + 32 + 64 + 128 + 256 + 2048 #Ignore velocities, accelertion and yaw rate
-### State machine definitions
-class MainState(Enum):
-    State_waiting_px       = 1
-    State_waiting_mark     = 2
-    State_waiting_lpos     = 3
-    State_waiting_offboard = 4
-    State_correcting       = 5 
-    State_landing          = 6
 
 ### Setup state variables
-curr_state = prev_state = MainState.State_waiting_px
+curr_state = prev_state = State_waiting_px
 
 ### 180 deg rotation matrix around the x axis
 R_flip  = np.zeros((3,3), dtype=np.float32)
@@ -154,20 +170,20 @@ def handle_heartbeat(msg):
         copter_mode = mavutil.mode_string_v10(msg)
 	#is_armed = msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED
 	#is_enabled = msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_GUIDED_ENABLED
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>MAV mode=",copter_mode)
+        #print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>MAV mode=",copter_mode)
 	  
 def handle_lpos(msg):
          global altitude_amsl, altitude_amsl_updated, lpos_received_time, lpos_data
          lpos_data = (msg.x, msg.y, msg.z, msg.vx, msg.vy, msg.vz)
          if msg.x<>0 and msg.y<>0:
               lpos_received_time = datetime.now() 
-              if opts.showmessages:
-                   print ("MSG type= LPOS")
-                   print "X\tY\tZ\tVx\tVy\tVz"
-                   print "%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f" % lpos_data
-         else:
-              if opts.showmessages:
-                   print ("LPOS - NO POSITION!")
+#              if opts.showmessages:
+#                   print ("MSG type= LPOS")
+#                   print "X\tY\tZ\tVx\tVy\tVz"
+#                   print "%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f" % lpos_data
+#         else:
+#              if opts.showmessages:
+#                   print ("LPOS - NO POSITION!")
 
 def process_mavlink():
     global master, msg, msg_type
@@ -193,7 +209,14 @@ def process_mavlink():
                    handle_lpos(msg)
          else:
               have_data = False  
-	
+
+def copter_change_mode(new_mode):
+    master.mav.set_mode_send(COPTER_SYS_ID, int(new_mode), 0)
+
+def copter_request_landing():
+    abcd=325
+    #master.mav.command_long_send(COPTER_SYS_ID, 0, mavutil.mavlink.MAV_CMD_NAV_LAND_LOCAL, 0, 2, 1, landing_target.yaw, landing_target.y, landing_target.x, landing_target.z, 0)    
+
 def process_camera():
     global cap, master, first_loop, opts, yaw_copter, marker_visible, app_should_stop, start_time, pos_camera_cm, observation_start
     ### Reset visibility flag
@@ -232,15 +255,15 @@ def process_camera():
          pos_camera_cm = -R_tc*np.matrix(tvec).T
          pitch_cam, yaw_cam, roll_cam = rotationMatrixToEulerAngles(R_flip*R_tc)
          ### Show information
-         if not opts.showmessages:
-              print ("Att pitch=%4.2f yaw=%4.2f roll=%4.2f" % (pitch_cam, yaw_cam, roll_cam))
+         #if opts.showmessages:
+         #     print ("Att pitch=%4.2f yaw=%4.2f roll=%4.2f" % (pitch_cam, yaw_cam, roll_cam))
 	 if not first_loop :
               if True:  #yaw_mark_detected and altitude_amsl_updated:
                    ### Calc copter yaw angle
                    yaw_copter= yaw_mark + yaw_cam
 		   ### Convert to NED and meters and include copter baro alt
 		   p_x_cm, p_y_cm = uav_to_ne(pos_camera_cm[2], pos_camera_cm[0], yaw_copter)
-                   p_z_m = 0.01 * pos_camera_cm[1]
+                   p_z_m = -0.01 * pos_camera_cm[1]
                    ### Send data to FC position is NED
 		   master.mav.vision_position_estimate_send(delta_time, p_x_cm * 0.01, p_y_cm * 0.01, p_z_m, roll_cam, pitch_cam, yaw_copter)
                    if opts.showmessages:
@@ -265,81 +288,100 @@ def process_camera():
               app_should_stop = True
 
 def correct_lpos():
+    global lpos_data, attitude_mav
+
     #if opts.showmessages:
-    #     print("--------------------------Call of correct_lpos")
+    #     print("Correcting LPOS")
+    
+    ### Calculate deltas
+    delta_x = prelanding_target.x - lpos_data[0]
+    delta_y = prelanding_target.y - lpos_data[1]
+    delta_z = prelanding_target.z - lpos_data[2]
+    delta_yaw = prelanding_target.yaw - attitude_mav[2]
 
-    new_x_m = -1.5
-    new_y_m = -0.2
-    new_z_m = -0.8
-    new_yaw = 0
+    ###Calculate corrected position
+    if delta_x > x_precision:
+         new_x_m = lpos_data[0] + delta_x * 0.1
+    else:
+         new_x_m = lpos_data[0] + delta_x
 
-    ###lpos.x, lpos.y, lpos.z - copter position in NED
-    ###attitude_mav[2] - yaw
+    if delta_y > y_precision:
+         new_y_m = lpos_data[1] + delta_y * 0.1
+    else:
+         new_y_m = lpos_data[1] + delta_y
 
-    ###pos_camera_cm - camera position 
-    ###yaw_cam - copter rotation from marker
-    ###master.mav.simple_goto(new_location)
+    if delta_z > z_precision:
+         new_z_m = lpos_data[2] + delta_z * 0.1
+    else:
+         new_z_m = lpos_data[2] + delta_z
+
+    if delta_z > yaw_precision:
+         new_z_m = attitude_mav[2] + delta_yaw * 0.1
+    else:
+         new_z_m = attitude_mav[2] + delta_yaw
 
     ### Prepare message components
     nm_time_boot_ms = delta_millis(start_boot)
     nm_sys_id = COPTER_SYS_ID
-    nm_sys_comp = 0
     nm_coordinate_frame = mavutil.mavlink.MAV_FRAME_LOCAL_NED
     nm_type_mask = LPOS_TYPE_MASK
     nm_x = new_x_m
     nm_y = new_y_m
     nm_z = new_z_m
-    nm_vx = 0.0
-    nm_vy = 0.0
-    nm_vz = 0.0
-    nm_afx = 0.0
-    nm_afy = 0.0
-    nm_afz = 0.0
     nm_yaw = new_yaw
-    nm_yawrate = 0.0
-    ### Send message
-    master.mav.set_position_target_local_ned_send(nm_time_boot_ms, nm_sys_id, nm_sys_comp, nm_coordinate_frame, nm_type_mask, nm_x, nm_y, nm_z, nm_vx, nm_vy, nm_vz, nm_afx, nm_afy, nm_afz, nm_yaw, nm_yawrate)
 
-def copter_change_mode(new_mode):
-     master.mav.set_mode_send(COPTER_SYS_ID, int(new_mode), 0)
+    ### Send message with following full format
+    #master.mav.set_position_target_local_ned_send(nm_time_boot_ms, nm_sys_id, nm_sys_comp, nm_coordinate_frame, nm_type_mask, nm_x, nm_y, nm_z, nm_vx, nm_vy, nm_vz, nm_afx, nm_afy, nm_afz, nm_yaw, nm_yawrate)
+    master.mav.set_position_target_local_ned_send(nm_time_boot_ms, nm_sys_id, 0, nm_coordinate_frame, nm_type_mask, nm_x, nm_y, nm_z, 0, 0, 0, 0, 0, 0, nm_yaw, 0)
+
+def check_for_landing():
+     global lpos_data, attitude_mav, landing_target
+     x_ok = abs(prelanding_target.x - lpos_data[0]) < x_precision
+     y_ok = abs(prelanding_target.y - lpos_data[1]) < y_precision
+     z_ok = abs(prelanding_target.z - lpos_data[2]) < z_precision
+     yaw_ok = abs(prelanding_target.yaw - attitude_mav[2]) < yaw_precision
+     if ( x_ok and y_ok and z_ok and yaw_ok ):
+         if opts.showmessages:
+              print ("OK for landing")
+         setState(State_landing)
 	
 def setState(new_state):
-    global curr_state, prev_state, MainState
-    if curr_state == MainState.State_waiting_mark:
+    global curr_state, prev_state
+    if curr_state == State_waiting_mark:
          ### do something		 
          if opts.showmessages:
               print("Call of setState p1")
-    elif curr_state == MainState.State_waiting_lpos:
-         if new_state == MainState.State_waiting_offboard:
-              #copter_change_mode(COPTER_MODE_GUIDED)
+    elif curr_state == State_waiting_lpos:
+         if new_state == State_waiting_offboard:
               copter_change_mode(mavutil.mavlink.MAV_MODE_GUIDED_DISARMED)
-         #print("+++++++++++++++++++++++++++++++++++++++OFFBOARD REQUEST!!!!!!!!!!!!!!1")
          ### do something
-    elif curr_state == MainState.State_waiting_offboard:
+    elif curr_state == State_waiting_offboard:
          ### do something
          if opts.showmessages:
-              print("+++++++++++++++++++++++++++++++++++++++Call of setState p2")
-    elif curr_state == MainState.State_correcting:
+              print("Call of setState p2")
+    elif curr_state == State_correcting:
          ### do something
          if opts.showmessages:
               print("Call of setState p3")
-    elif curr_state == MainState.State_landing:
+         if new_state == State_landing:
+              copter_request_landing()
+    elif curr_state == State_landing:
          ### do something
 	 ### update states variables
 	 prev_state = curr_state
     #Set state for next iterratoins
     curr_state = new_state
-    print("+++++++++++++++NEW STATE=", curr_state)
+    print("State changed, NEW STATE=", curr_state)
 
 def waiting_mark():
-    global curr_state, cap, master, first_loop, opts, yaw_copter, msg, msg_type, marker_visible, MainState
+    global curr_state, cap, master, first_loop, opts, yaw_copter, msg, msg_type, marker_visible
     ### process communication with PX4
     process_mavlink()
     ### process video
     process_camera()
     ### If observation is stable - switch to next state
     if marker_visible and delta_millis(observation_start) > MIN_OBSERVATION_PERIOD :
-         setState(MainState.State_waiting_lpos)
+         setState(State_waiting_lpos)
 	
 def waiting_lpos():
     global curr_state, cap, master, first_loop, opts, yaw_copter, msg, msg_type, marker_visible
@@ -350,7 +392,7 @@ def waiting_lpos():
     ### Check for switch to next state
     if lpos_received_time:
          if  delta_millis(lpos_received_time) < LPOS_OBSERVATION_MILLIS:
-    	      setState(MainState.State_waiting_offboard)
+    	      setState(State_waiting_offboard)
 
 def waiting_offboard():
     global curr_state, cap, master, first_loop, opts, yaw_copter, msg, msg_type, marker_visible, copter_mode
@@ -362,25 +404,26 @@ def waiting_offboard():
     copter_change_mode(mavutil.mavlink.MAV_MODE_GUIDED_DISARMED)
     ### Reset mode if no mark visible
     if  delta_millis(lpos_received_time) > LPOS_OBSERVATION_MILLIS:
-         setState(MainState.State_waiting_lpos)
+         setState(State_waiting_lpos)
          copter_change_mode(mavutil.mavlink.MAV_MODE_MANUAL_ARMED)  
 
     ### process setpoint calculation
     if copter_mode == "POSCTL":
-         setState(MainState.State_correcting)
+         setState(State_correcting)
 		 
 def do_correcting():
-    global curr_state, cap, master, first_loop, opts, yaw_copter, msg, msg_type, marker_visible
+    global curr_state, cap, master, first_loop, opts, yaw_copter, msg, msg_type, marker_visible, lpos, attitude_mav, landing_target
     ### process communication with PX4
     process_mavlink()
     ### process video
     process_camera()
     ### calc correction
     correct_lpos()
+    ### check fo landing position
+    check_for_landing()
     ### Reset mode if no mark
     if  delta_millis(lpos_received_time) > LPOS_OBSERVATION_MILLIS:
-         setState(MainState.State_waiting_lpos)
-         #copter_change_mode(COPTER_MODE_MANUAL)
+         setState(State_waiting_lpos)
          copter_change_mode(mavutil.mavlink.MAV_MODE_MANUAL_ARMED)  
 
 def do_landing():
@@ -389,6 +432,8 @@ def do_landing():
     process_mavlink()
     ### process video
     process_camera()
+    ### calc correction
+    correct_lpos()
 
 ### Get the camera calibration path
 camera_matrix = np.loadtxt(calib_path + calibration_file, delimiter=',')
@@ -409,18 +454,12 @@ font = cv2.FONT_HERSHEY_PLAIN
 
 ### read command line options
 parser = OptionParser("readdata.py [options]")
-parser.add_option("--baudrate", dest="baudrate", type='int',
-				  help="master port baud rate", default=115200)
+parser.add_option("--baudrate", dest="baudrate", type='int', help="master port baud rate", default=115200)
 parser.add_option("--device", dest="device", default=None, help="serial device")
 parser.add_option("--rate", dest="rate", default=4, type='int', help="requested stream rate")
-parser.add_option("--source-system", dest='SOURCE_SYSTEM', type='int',
-				  default=255, help='MAVLink source system for this GCS')
-parser.add_option("-v", "--verbose",
-                  action="store_true", dest="showmessages", default=False,
-                  help="Print status messages to stdout")
-parser.add_option("-s", "--show",
-                  action="store_true", dest="showvideo", default=False,
-                  help="Show video from camera to frame")
+parser.add_option("--source-system", dest='SOURCE_SYSTEM', type='int', default=255, help='MAVLink source system for this GCS')
+parser.add_option("-v", "--verbose", action="store_true", dest="showmessages", default=False, help="Print status messages to stdout")
+parser.add_option("-s", "--show", action="store_true", dest="showvideo", default=False, help="Show video from camera to frame")
 parser.add_option("--distance", dest="distance", default=None, help="landing distance from aruco mark")
 
 (opts, args) = parser.parse_args()
@@ -441,7 +480,7 @@ if opts.showmessages:
 master.wait_heartbeat()
 
 # Switch to next state
-setState(MainState.State_waiting_mark)
+setState(State_waiting_mark)
 
 #Stich copter to manual mode
 copter_change_mode(mavutil.mavlink.MAV_MODE_MANUAL_ARMED)
@@ -464,15 +503,15 @@ while True:
     ###
     start_time = datetime.now()
 
-    if curr_state == MainState.State_waiting_mark:
+    if curr_state == State_waiting_mark:
          waiting_mark()
-    elif curr_state == MainState.State_waiting_lpos:
+    elif curr_state == State_waiting_lpos:
 	 waiting_lpos()
-    elif curr_state == MainState.State_waiting_offboard:
+    elif curr_state == State_waiting_offboard:
 	 waiting_offboard()
-    elif curr_state == MainState.State_correcting:
+    elif curr_state == State_correcting:
 	 do_correcting()
-    elif curr_state == MainState.State_landing:
+    elif curr_state == State_landing:
 	 do_landing()
     ##Check for exit from application flag
     if app_should_stop == True: